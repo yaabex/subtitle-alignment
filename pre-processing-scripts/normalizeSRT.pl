c442d0313eb5ce60fefaedb4beaaf00327e902bc
b2rosado <b2rosado@gmail.com>
2016-01-11 17:22:57 +0000

    Added pre-processing-scripts

---
+++ pre-processing-scripts/normalizeSRT.pl
@@ -0,0 +1,57 @@
+use 5.010;
+use strict;
+use warnings;
+
+my $filename = $ARGV[0];
+my $file;
+open($file, '<:encoding(iso-8859-1)', $filename)
+#open($file, '<:encoding(UTF-8)', $filename)
+or
+die "ERROR: Could not open file '$filename' $!";
+
+my $text = "";
+my $header = "";
+
+sub printText{
+	if($text =~ m/^$/){				#if empty text, do not print header
+		$header = "";
+	}else{
+		print $header;
+		$header = "";
+		$text =~ s/([\.]{3}|[^.!?])$/$1>>/g;
+		print $text . "\n\n";
+		$text = "";
+	}
+}
+
+sub appendText{
+	chomp($_[0]);
+
+	$text = $text . " " . $_[0];
+	$text =~ s/([\.]+ )[\.]+/$1/g;
+	$text =~ s/^ //g;
+}
+
+foreach my $line (<$file>){
+	$line =~ s/<[\/\w]+>//g;				# remove tags
+	$line =~ s/\[?[A-Z].+\]?\n?.+[A-Z ]{7,}.+\]?//g;	# remove DESCRIPTIONS
+	$line =~ s/\r//g;
+
+	if(	$line =~ m/^[0-9]+$/ or		# id line
+		$line =~ m/-->/){			# timestamp line
+		$header = $header.$line;	# store header
+
+	}else{
+		if($line =~ m/^$/){			# newline
+			printText();			# print entire block
+		}else{
+			appendText($line);		# there is text to append
+		}
+	}
+}
+printText();
+close($file);
+
+#TODO
+	# separar frases diferentes na mesma linha (. -)
+	# juntar frases em linhas diferentes mas com ... 
